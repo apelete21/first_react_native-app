5de7d5186c0a89ce7e246bc0ec104bb9aefa4468
Apelete <adzohonouapelete@gmail.com>
2023-03-23 14:17:26 +0000

    initial_commit_with_starter_files

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import {Colors, Fonts, Sizes} from '../constants'
+
+export default function Home() {
+        
+  return (
+    <View>
+      <Text>Home Page</Text>
+    </View>
+  );
+}
